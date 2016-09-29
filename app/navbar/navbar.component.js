30202894542a5efae813a194b9ca669c74eca4c3
ewgo <ewelina.gonda@gft.com>
2016-09-29 12:34:22 +0200

    tidies

--- app/navbar/navbar.component.js
+++ app/navbar/navbar.component.js
@@ -2,16 +2,8 @@ angular.module('navbar').component('navbar', {
     templateUrl: 'navbar/navbar.template.html',
     controller: [function () {
         var vm = this;
-        // $http.get('/menu').then(function (response) {
-        //     vm.menu = response.data;
-        // });
 
         vm.isActive = false;
-        vm.showedMenu = true;
-        vm.iconClass= 'col-md-2';
 
-        vm.toggleMenu = function () {
-            vm.showedMenu = !vm.showedMenu;
-        };
     }]
 });
