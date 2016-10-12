a65e202449e83c751f860edb0530bd4aaacfabc6
ewgo <ewelina.gonda@gft.com>
2016-10-12 17:04:47 +0200

    started ui-router

--- app/app.config.js
+++ app/app.config.js
@@ -1,16 +1,49 @@
 angular.module('app').
-config(['$locationProvider', '$routeProvider',
-    function config($locationProvider, $routeProvider){
+    config(['$locationProvider', '$routeProvider',
+        function config($locationProvider, $routeProvider) {
 
-        $routeProvider.
-            when('/signin', {
-                template: '<login-screen></login-screen>'
-            }).
-            when('/home', {
-                template: '<dashboard></dashboard>'
-            }).
-            when('/error', {
-                template: '<error500></error500>'
-            }).
-            otherwise('/signin');
-    }]);
+            $routeProvider.
+                when('/signin', {
+                    template: '<login-screen></login-screen>'
+                }).
+                when('/home', {
+                    template: '<dashboard></dashboard>'
+                }).
+                when('/error', {
+                    template: '<error500></error500>'
+                }).
+                otherwise('/signin');
+        }]);
+
+
+
+
+
+angular.module('app').
+    config(['$stateProvider', '$urlRouterProvider',
+        function ($stateProvider, $urlRouterProvider) {
+
+            $stateProvider
+                .state('inside', {
+                    // url: '/bulletinBoard',
+                    views: {
+                        'header': {
+                            component: 'top-nav'
+                        },
+                        'menu': {
+                            component: 'navbar'
+                        }
+                    },
+                    // templateUrl: '../src/app/bulletinBoard/views/bulletinBoard.part.html'
+                }).state('inside.dashboard', {
+                    url: 'home',
+                    views: {
+                        content: {
+                            component: 'dashboard'
+                        }
+                    }
+                })
+
+            $urlRouterProvider.otherwise('/');
+        }
+    ]);
--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,7 @@ gulp.task('index', function () {
       "./node_modules/angular-animate/angular-animate.min.js",
       "./node_modules/jquery/dist/jquery.min.js",
       "./node_modules/bootstrap/dist/js/bootstrap.min.js",
+      "./node_modules/angular-ui-router/release/angular-ui-router.min.js",
       "./app/app.module.js",
       "./app/app.config.js",
       "./app/app.controller.js",
